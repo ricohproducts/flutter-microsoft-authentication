31faec0545c73cffb28d42a5123dfcf93536e604
Robert Burgess <rob.a.burgess@gmail.com>
2024-05-17 15:43:48 +0100

    main

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,25 +14,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   String _graphURI = "https://graph.microsoft.com/v1.0/me/";
 
   String _authToken = 'Unknown Auth Token';
   String _username = 'No Account';
   String _msProfile = 'Unknown Profile';
 
-  FlutterMicrosoftAuthentication fma;
+  late FlutterMicrosoftAuthentication fma;
 
   @override
   void initState() {
     super.initState();
 
     fma = FlutterMicrosoftAuthentication(
-      kClientID: "<client-id>",
-      kAuthority: "https://login.microsoftonline.com/organizations",
-      kScopes: ["User.Read", "User.ReadBasic.All"],
-      androidConfigAssetPath: "assets/android_auth_config.json"
-    );
+        kClientID: "<client-id>",
+        kAuthority: "https://login.microsoftonline.com/organizations",
+        kScopes: ["User.Read", "User.ReadBasic.All"],
+        androidConfigAssetPath: "assets/android_auth_config.json");
     print('INITIALIZED FMA');
   }
 
@@ -40,7 +38,7 @@ class _MyAppState extends State<MyApp> {
     String authToken;
     try {
       authToken = await this.fma.acquireTokenInteractively;
-    } on PlatformException catch(e) {
+    } on PlatformException catch (e) {
       authToken = 'Failed to get token.';
       print(e.message);
     }
@@ -53,7 +51,7 @@ class _MyAppState extends State<MyApp> {
     String authToken;
     try {
       authToken = await this.fma.acquireTokenSilently;
-    } on PlatformException catch(e) {
+    } on PlatformException catch (e) {
       authToken = 'Failed to get token silently.';
       print(e.message);
     }
@@ -66,7 +64,7 @@ class _MyAppState extends State<MyApp> {
     String authToken;
     try {
       authToken = await this.fma.signOut;
-    } on PlatformException catch(e) {
+    } on PlatformException catch (e) {
       authToken = 'Failed to sign out.';
       print(e.message);
     }
@@ -80,12 +78,12 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _username = username;
     });
+    return username;
   }
 
   _fetchMicrosoftProfile() async {
-    var response = await http.get(this._graphURI, headers: {
-      "Authorization": "Bearer " + this._authToken
-    });
+    var response = await http.get(Uri.parse(this._graphURI),
+        headers: {"Authorization": "Bearer " + this._authToken});
 
     setState(() {
       _msProfile = json.decode(response.body).toString();
@@ -96,39 +94,46 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Microsoft Authentication'),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                RaisedButton( onPressed: _acquireTokenInteractively,
-                  child: Text('Acquire Token'),),
-                RaisedButton( onPressed: _acquireTokenSilently,
-                    child: Text('Acquire Token Silently')),
-                RaisedButton( onPressed: _signOut,
-                    child: Text('Sign Out')),
-                RaisedButton( onPressed: _fetchMicrosoftProfile,
-                    child: Text('Fetch Profile')),
-                if (Platform.isAndroid == true)
-                  RaisedButton( onPressed: _loadAccount,
-                      child: Text('Load account')),
-                SizedBox(height: 8,),
-                if (Platform.isAndroid == true)
-                  Text( "Username: $_username"),
-                SizedBox(height: 8,),
-                Text( "Profile: $_msProfile"),
-                SizedBox(height: 8,),
-                Text( "Token: $_authToken"),
-              ],
-            ),
+          appBar: AppBar(
+            title: const Text('Microsoft Authentication'),
           ),
-        )
-      ),
+          body: SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _acquireTokenInteractively,
+                    child: Text('Acquire Token'),
+                  ),
+                  ElevatedButton(
+                      onPressed: _acquireTokenSilently,
+                      child: Text('Acquire Token Silently')),
+                  ElevatedButton(onPressed: _signOut, child: Text('Sign Out')),
+                  ElevatedButton(
+                      onPressed: _fetchMicrosoftProfile,
+                      child: Text('Fetch Profile')),
+                  if (Platform.isAndroid == true)
+                    ElevatedButton(
+                        onPressed: _loadAccount, child: Text('Load account')),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  if (Platform.isAndroid == true) Text("Username: $_username"),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text("Profile: $_msProfile"),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text("Token: $_authToken"),
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
